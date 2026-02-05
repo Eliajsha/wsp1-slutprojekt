5407faeab36fbed1da0344ca9f5991fb7dc93073
Eliajsha <elias.svensson1@yahoo.se>
2026-02-05 16:55:17 +0100

    Börjat fixa databas
    börjat lite

--- db/seeder.rb
+++ db/seeder.rb
@@ -7,3 +7,20 @@ class Seeder
   end
 
 end
+
+def self.drop_tables
+  db.execute('DROP TABLE IF EXISTS recipes')
+end
+
+def self.create_tables
+  db.execute('CREATE TABLE recipes (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              recipe_name TEXT NOT NULL,
+              recipe TEXT NOT NULL,
+              user_id INTEGER,
+              category_id INTEGER)')
+end
+
+def self.populate_tables
+  db.excecute('INSERT INTO recipes(recipe_name, recipe, user_id, category_id) VALUES (Pannkakor, baka på för fan)')
+end
