68499496a9ad069042f7b136c4fa188523800d1f
Eliajsha <elias.svensson1@yahoo.se>
2026-03-26 16:50:10 +0100

    Har nästan gjort klart login helt

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'debug'
 require "awesome_print"
+require 'sinatra'
+require 'securerandom'
+
 
 class App < Sinatra::Base
 
@@ -15,6 +18,18 @@ class App < Sinatra::Base
       return @db
     end
 
+    configure do
+      enable :sessions
+      set :session_secret, SecureRandom.hex(64)
+    end
+
+    before do
+      if session[:user_id]
+        @current_user = db.execute("SELECT * FROM users WHERE id = ?", session[:user_id]).first
+        ap @current_user
+      end
+    end
+
     # Routen / 
     get '/' do
         erb(:"recipes/index")
@@ -23,12 +38,12 @@ class App < Sinatra::Base
     get '/recipes' do
       @recipes = db.execute("SELECT * FROM recipes")
       p @recipes
-      erb(:"main/index")
+      erb(:"recipes/index")
     end
 
     get '/recipes/:id' do | id |
       @recipes = db.execute("SELECT * FROM recipes WHERE id=?", id).first
-      erb(:"main/show")
+      erb(:"recipes/show")
     end
 
     get '/login' do 
@@ -39,5 +54,27 @@ class App < Sinatra::Base
       request_username = params[:username]
       request_plain_password = params[:password]
       
-      user = db.execute("SELECT * FROM users WHERE username=?", request_username).first
+      user = db.execute("SELECT * FROM users WHERE username = ?", request_username).first
+
+      unless user
+        ap "/login : Invalid username."
+        status 401
+        redirect '/login'
+      end
+
+      db_id = user["id"].to_i
+      db_password_hashed = user["password"].to_s
+
+      bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+
+      if bcrypt_db_password == request_plain_password
+        ap "/login : Logged in -> redirecting to your recipes"
+        session[:user_id] = db_id
+        redirect "/user_recipes"
+      else
+        ap "/login : Invalid password."
+        status 401
+        redirect '/login'
+      end
+    end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,71 +3,73 @@ require_relative '../config'
 require 'bcrypt'
 
 
+
 class Seeder
 
-  def self.seed!
-    puts "Using db file: #{DB_PATH}"
-    puts "🧹 Dropping old tables..."
-    drop_tables
-    puts "🧱 Creating tables..."
-    create_tables
-    puts "🍎 Populating tables..."
-    populate_tables
-    puts "✅ Done seeding the database!"
+    def self.seed!
+      puts "Using db file: #{DB_PATH}"
+      puts "🧹 Dropping old tables..."
+      drop_tables
+      puts "🧱 Creating tables..."
+      create_tables
+      puts "🍎 Populating tables..."
+      populate_tables
+      puts "✅ Done seeding the database!"
+    end
+
+
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS recipes')
   end
 
+  def self.create_tables
+    db.execute('CREATE TABLE recipes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                recipe_name TEXT NOT NULL,
+                description TEXT NOT NULL,
+                user_id INTEGER,
+                category TEXT NOT NULL,
+                rating TEXT NOT NULL)')
+  end
 
-def self.drop_tables
-  db.execute('DROP TABLE IF EXISTS recipes')
-end
+  def self.populate_tables
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Pannkakor", "baka på för fan", 2, "vegetariskt", "★★★★☆")')
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Köttbullar med potatismos", "Du köttar köttet till en boll o sen lagar köttet till en köttbull o drämmer på potätmos med brunsås", 1, "Animaliskt", "★★★★★")')
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Cesarsallad", "Blanda ihop lite grönsaker och en fet kyckling med sös o dammish", 1, "Animaliskt", "★★★☆☆")')
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Grönsakswok", "Stek upp lite grönsaker och servera med ris", 2, "Vegetariskt", "★★☆☆☆")')
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Lasagne", "Varva köttfärssås och bechamelsås och ost och pasta och sen in i ugnen", 1, "Animaliskt", "★★★★☆")')
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Falafel", "Mixa ihop lite kikärtor och kryddor och gör bollar av det och fritera", 2, "Vegetariskt", "★☆☆☆☆")')
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Sushi", "Rulla ihop lite ris och fisk och grönsaker i ett noriark", 4, "Animaliskt", "★★★★★")')
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Vegetarisk lasagne", "Varva grönsaksfärssås och bechamelsås och ost och pasta och sen in i ugnen", 2, "Vegetariskt", "★★★★☆")')
+    db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Grönsaksgryta", "Koka ihop lite grönsaker i en god buljong", 4, "Veganskt", "★★★☆☆")')
+  end
 
-def self.create_tables
-  db.execute('CREATE TABLE recipes (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              recipe_name TEXT NOT NULL,
-              description TEXT NOT NULL,
-              user_id INTEGER,
-              category TEXT NOT NULL,
-              rating TEXT NOT NULL)')
-end
+  private
 
-def self.populate_tables
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Pannkakor", "baka på för fan", 2, "vegetariskt", "★★★★☆")')
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Köttbullar med potatismos", "Du köttar köttet till en boll o sen lagar köttet till en köttbull o drämmer på potätmos med brunsås", 1, "Animaliskt", "★★★★★")')
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Cesarsallad", "Blanda ihop lite grönsaker och en fet kyckling med sös o dammish", 1, "Animaliskt", "★★★☆☆")')
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Grönsakswok", "Stek upp lite grönsaker och servera med ris", 2, "Vegetariskt", "★★☆☆☆")')
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Lasagne", "Varva köttfärssås och bechamelsås och ost och pasta och sen in i ugnen", 1, "Animaliskt", "★★★★☆")')
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Falafel", "Mixa ihop lite kikärtor och kryddor och gör bollar av det och fritera", 2, "Vegetariskt", "★☆☆☆☆")')
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Sushi", "Rulla ihop lite ris och fisk och grönsaker i ett noriark", 4, "Animaliskt", "★★★★★")')
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Vegetarisk lasagne", "Varva grönsaksfärssås och bechamelsås och ost och pasta och sen in i ugnen", 2, "Vegetariskt", "★★★★☆")')
-  db.execute('INSERT INTO recipes(recipe_name, description, user_id, category, rating) VALUES ("Grönsaksgryta", "Koka ihop lite grönsaker i en god buljong", 4, "Veganskt", "★★★☆☆")')
-end
+  def self.db
+    @db ||= begin
+      db = SQLite3::Database.new(DB_PATH)
+      db.results_as_hash = true
+      db
+    end
+  end
 
-private
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS users')
+  end
 
-def self.db
-  @db ||= begin
-    db = SQLite3::Database.new(DB_PATH)
-    db.results_as_hash = true
-    db
+  def self.create_tables
+    db.execute('CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL)')
+  end
+
+  def self.populate_tables
+    password_hashed = BCrypt::Password.create("123")
+    p "Storing hashed password (#{password_hashed}) to DB. Clear text password (123) never saved"
+    db.execute('INSERT INTO users(username, password) VALUES (?, ?)', ["Elias", password_hashed])
   end
 end
-end
-
-def self.drop_tables
-  db.execute('DROP TABLE IF EXISTS users')
-end
-
-def self.create_tables
-  db.execute('CREATE TABLE users (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              username TEXT NOT NULL,
-              password TEXT NOT NULL)')
-end
-
-def self.populate_tables
-  db.execute('INSERT INTO users(username, password) VALUES ("Elias", "123")')
-end
-
 
 Seeder.seed!
